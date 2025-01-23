7658eedb6e1297ced289d0b49d795db981e9d465
Ivan <ivan.kalyagin50@gmail.com>
2025-01-24 00:42:29 +0300

    Initial Commit

---
+++ appcreator/appcreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  appcreatorApp.swift
+//  appcreator
+//
+//  Created by Ivan on 24.01.25.
+//
+
+import SwiftUI
+
+@main
+struct appcreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ appcreatorTests/appcreatorTests.swift
@@ -0,0 +1,16 @@
+//
+//  appcreatorTests.swift
+//  appcreatorTests
+//
+//  Created by Ivan on 24.01.25.
+//
+
+import Testing
+
+struct appcreatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
